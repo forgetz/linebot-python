ae58481bad3eeb38b080f0c90f3f7358c290609e
saranpong <creadz@gmail.com>
2019-07-20 17:35:53 +0700

    first init

---
+++ projects/__init__.py
@@ -0,0 +1,15 @@
+from flask import Flask, request, abort
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+   return 'hello world', 200
+
+@app.route('/webhook', methods=['POST','GET'])
+def webhook():
+   if request.method == 'POST':
+      print(request.json)
+      return request.json, 200
+   elif request.method == 'GET':
+      return 'hook', 200
